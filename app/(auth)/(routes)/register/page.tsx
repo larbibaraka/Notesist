e0c2cd0cb6fb227a8a706bbfe80f5fff15fa71d8
larbibaraka <larbibaraka@gmail.com>
2023-12-03 18:48:00 +0100

    add icon

--- app/(auth)/(routes)/register/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const registerPage = () => {
-  return <div>registerPage</div>;
-};
-
-export default registerPage;
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <Button variant="outline">click me</Button>
+      <Button variant="destructive">click me</Button>
     </div>
   );
 }
