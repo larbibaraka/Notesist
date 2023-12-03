41c759c74d81d1224483e5e25ccfc06de2108f15
larbibaraka <larbibaraka@gmail.com>
2023-12-03 18:58:54 +0100

    create heading component

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,12 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import Heading from "./_componenets/heading";
 
-export default function Home() {
+function MarketingPage() {
   return (
-    <div>
-      <Button variant="destructive">click me</Button>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+      </div>
     </div>
   );
 }
+export default MarketingPage;
